2ff3e2bfaeb6545f7704a81eddda92777b65d2c8
plantain-00 <y.yao@hugegis.com>
2014-06-27 17:22:52 +0800

    add example and nuget

---
+++ ExcelFile.net.Example.net40/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ExcelFile.net.Example.net40
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var excel = new ExcelFile();
+            excel.Sheet("test sheet");
+            excel.Row().Cell("test1").Cell(2);
+            excel.Row().Cell("test2").Cell(3);
+            excel.Save("a.xls");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,10 @@ ExcelFile.net
 =============
 
 A Excel File Writer based on NPOI.
+
+    excel.Sheet("test sheet");
+    excel.Row().Cell("test1").Cell(2);
+    excel.Row().Cell("test2").Cell(3);
+    excel.Save("a.xls");
+
+You can get [it](https://www.nuget.org/packages/ExcelFile.net) from Nuget.
